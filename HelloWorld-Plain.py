79a839174c82f3a45fa3a8a9cd783d8fc83fe00e
Suneel Padavala <sumadhish.padavala@gmail.com>
2023-10-06 09:41:05 -0700

    Updated the filename references

--- HelloWorld-Plain.py
+++ HelloWorld-Plain.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_talisman import Talisman
+
 app = Flask(__name__)
+talisman = Talisman(app)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
